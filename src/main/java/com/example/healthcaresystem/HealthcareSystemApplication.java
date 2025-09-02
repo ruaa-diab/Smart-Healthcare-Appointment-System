4eac2edcf73adf84cc827a7783d749f1e56fb28b
Ruaa Diab <Ruaa.Diab@exalt.ps>
2025-09-02 11:18:50 +0300

    initial commit

---
+++ src/main/java/com/example/healthcaresystem/HealthcareSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.healthcaresystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcareSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthcareSystemApplication.class, args);
+    }
+
+}
